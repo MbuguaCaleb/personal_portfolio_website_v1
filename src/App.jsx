e7f4b0ee4fac2c89ecfed5c34b13ebe1fc346bdc
Mbugua Caleb <mbuguacaleb30@gmail.com>
2021-11-26 17:44:12 +0300

    header-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Topbar from './components/topbar/Topbar'
+import Menu from './components/menu/Menu'
 import Intro from './components/intro/Intro'
 import Portfolio from './components/portfolio/Portfolio'
 import Works from './components/works/Works'
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className='app'>
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className='sections'>
         <Intro />
         <Portfolio />
