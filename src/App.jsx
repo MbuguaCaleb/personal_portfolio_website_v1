2e99b4c3553e38e37dd18b86b8185ac968bbf4ce
Mbugua Caleb <mbuguacaleb30@gmail.com>
2021-11-26 14:07:31 +0300

    beginning-my-portfolio

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Topbar from './components/topbar/Topbar'
+
+function App() {
+  return (
+    <div className='App'>
+      <Topbar />
+      <div className='section'>
+
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
