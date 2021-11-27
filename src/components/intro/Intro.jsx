735fe33deddc6ada23e69d2dcada54d159b98780
Mbugua Caleb <mbuguacaleb30@gmail.com>
2021-11-27 07:04:58 +0300

    intro-section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,36 @@
 import './intro.scss'
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
 
 export default function Intro() {
+  const textRef = useRef()
+  useEffect(() => {
+    init(textRef.current, {
+      backDelay: 1500,
+      backSpeed: 60,
+      showCursor: true,
+      strings: ['Engineer', 'Designer', 'Developer'],
+    })
+  }, [console.log(textRef)])
   return (
     <div className='intro' id='intro'>
-      intro
+      <div className='left'>
+        <div className='imgContainer'>
+          <img src='assets/my_profile_one.jpg' alt='' />
+        </div>
+      </div>
+      <div className='right'>
+        <div className='wrapper'>
+          <h2>Hi there, I'm</h2>
+          <h1>Mbugua Caleb</h1>
+          <h3>
+            Software <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href='#portfolio'>
+          <img src='assets/down.png' alt='' />
+        </a>
+      </div>
     </div>
   )
 }
