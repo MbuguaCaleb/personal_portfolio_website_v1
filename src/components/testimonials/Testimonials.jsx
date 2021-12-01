53313d31ad1e34b4e1f8e34b9f44880f97e071a2
Mbugua Caleb <mbuguacaleb30@gmail.com>
2021-12-01 15:01:40 +0300

    completing-the-testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,33 +1,46 @@
 import './testimonials.scss'
 
 function Testimonials() {
+  const data = [
+    {
+      id: 1,
+      name: 'Zack Githinji',
+      title: 'Team Leader & CEO Wizglobal Kenya',
+      img: 'assets/testimonials/zack_githinji.jpg',
+      icon: 'assets/linkedin.png',
+      desc: 'Kudos! It is incredible how thorough your work is #GoingAboveAndBeyond',
+      response:
+        "Thank you sir! You are the best team Lead..Right from internship up to now, you've guided us so well! Blessed to be in such a brilliant team!! 💪",
+      featured: true,
+    },
+  ]
   return (
     <div className='testimonials' id='testimonials'>
       <h1>Testimonials</h1>
 
       <div className='container'>
-        <div className='card'>
-          <div className='top'>
-            <img src='assets/right-arrow.png' className='left' alt='' />
+        {data.map((p) => {
+          return (
+            <div className={p.featured ? 'card featured' : 'card'} key={p.id}>
+              <div className='top'>
+                <img src={p.icon} className='left' alt='' />
 
-            <img
-              className='user'
-              src='https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
-              alt=''
-            />
+                <img className='user' src={p.img} alt='' />
 
-            <img className='right' src='assets/youtube.png' alt='' />
-          </div>
+                <img className='right' src='assets/youtube.png' alt='' />
+              </div>
 
-          <div className='center'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint ex
-          </div>
+              <div className='center'>{p.desc}</div>
 
-          <div className='bottom'>
-            <h3>Alex</h3>
-            <h4>CEO Of Microsoft </h4>
-          </div>
-        </div>
+              <div className='center'>{p.response}</div>
+
+              <div className='bottom'>
+                <h3>{p.name}</h3>
+                <h4>{p.title} </h4>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
